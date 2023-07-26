9b1b2b8b8ab28254463e24c56e2e939a78f387fc
Joseph Pierce <josephpierce215@gmail.com>
2023-07-25 21:08:10 -0700

    made it so you can't desesct the radio button by clicking again

--- radio_script.js
+++ radio_script.js
@@ -5,14 +5,7 @@ let selectedRadio = null;
 // Add a click event listener to each radio button
 radios.forEach(radio => {
     radio.addEventListener('click', () => {
-        if (radio === selectedRadio) {
-            // If the clicked radio button is already selected, deselect it
-            radio.checked = false;
-            selectedRadio = null;
-        } else {
-            // If the clicked radio button is not selected, select it
-            selectedRadio = radio;
-        }
+        selectedRadio = radio;
     });
 });
 
