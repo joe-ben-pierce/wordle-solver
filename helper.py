f046ce684293c60c427012c97a123b81eaa7ede2
Joseph Pierce <josephpierce215@gmail.com>
2023-09-12 21:12:31 -0700

    used more corpuses to improve the ordering of the word list

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 def put_wordlist_in_js_file(input_txt):
     # open the text file
     with open(input_txt, 'r') as f:
@@ -5,16 +7,21 @@ def put_wordlist_in_js_file(input_txt):
         lines = f.read().splitlines()
 
     # format the list as a JavaScript array
-    js_array = 'let valid_words = ' + str(lines) + ';'
+    js_array = 'let valid_words = ' + str(lines)
 
     # open the JS file
     with open('valid_words.js', 'w') as f:
         # write the JS array to the file
         f.write(js_array)
+        f.write(";\n\n")
+        f.write("function get_valid_words(){\n")
+        f.write("    return valid_words;\n")
+        f.write("}\n")
 
 
 def get_word_counts():
-    from nltk.corpus import reuters
+    from nltk.corpus import reuters, brown, webtext, movie_reviews, treebank, twitter_samples
+
     from collections import Counter
     import csv
 
@@ -23,7 +30,9 @@ def get_word_counts():
         words = f.read().splitlines()
 
     # Count word frequencies in the corpus
-    word_freqs = Counter(reuters.words())
+    word_freqs = Counter()
+    for corpus in [reuters, brown, webtext, movie_reviews, treebank]:
+        word_freqs.update(word.lower() for word in corpus.words())
 
     # For each word in your list, get its frequency (default to 0 if not found)
     word_counts = [(word, word_freqs[word]) for word in words]
@@ -47,4 +56,12 @@ def write_ordered_words(input_csv, output_txt):
         with open(output_txt, 'w') as out_file:
             for row in reader:
                 out_file.write(row[0] + '\n')
+
+def main():
+    list_file_name = "sorted-words.txt"
+    get_word_counts()
+    write_ordered_words("word_frequencies.csv", list_file_name)
+    put_wordlist_in_js_file(list_file_name)
+
+main()
 
